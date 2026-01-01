70eac56d4871b6eab8e8fe0cd40fab6336447407
codebyshyam-cmyk <codebyshyam@gmail.com>
2026-01-01 17:48:14 +0530

    room message function

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -29,5 +29,6 @@ class Message(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     
+    
     def __str__(self):
         return self.body
